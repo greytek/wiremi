5d9a90407e31711b37c53bd7c89d38530276c86c
GreyTek <i180601@nu.edu.pk>
2023-05-18 05:57:19 -0700

    1.0

--- db/conn.py
+++ db/conn.py
@@ -1,1 +1,6 @@
-connection_string = 'DRIVER={SQL Server};Server=localhost\SQLEXPRESS;Database=wiremi;Trusted_Connection=True;'
+# connection_string = 'DRIVER={SQL Server};Server=localhost\SQLEXPRESS;Database=wiremi;Trusted_Connection=True;'
+connection_string ="Driver={ODBC Driver 17 for SQL Server};" \
+                   "Server=wiremi.database.windows.net;" \
+                   "Database=wiremi-db;Uid=sqladmin;Pwd=Qwerty123;" \
+                   "Encrypt=yes;" \
+                   "TrustServerCertificate=no;Connection Timeout=30;"
